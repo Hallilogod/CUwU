dbea6110169cb9b40e97788cb9856182d40377c5
Hallilo <129677832+Hallilogod@users.noreply.github.com>
2024-02-16 21:34:02 +0000

    uwu

---
+++ CUwU.h
@@ -0,0 +1,134 @@
+typedef void uwuvoid, voiduwu, voiyd;
+typedef char charuwu, uwuchar, chnyar, chyar;
+typedef short shortuwu, uwushort, shwort; 
+typedef int uwuint, intuwu, uwint, wint;
+typedef long uwulong, longuwu, wong, wlong;
+typedef float uwufloat, floatuwu, fwoat;
+typedef double uwudouble, doubleuwu, doubwe, dowoble, dwouble;
+
+#define uwuauto auto
+#define autouwu auto
+#define auwto auto
+#define nyauto auto
+#define auwo auto
+
+#define uwuconst const
+#define constuwu const
+#define cwonst const
+#define cowonst const
+#define cwonwst const
+
+#define uwusigned signed
+#define signeduwu signed
+#define signyed signed
+
+#define uwuunsigned unsigned
+#define unsigneduwu unsigned
+#define unsignyed unsigned
+#define wunsigned unsigned
+
+#define uwustatic static
+#define staticuwu static
+#define stawtic static
+
+#define uwuregister register
+#define registeruwu register
+#define regwister register
+#define wregister register
+
+#define uwuvolatile volatile
+#define volatileuwu volatile
+#define vowatiwe volatile
+#define wowatile volatile
+#define owovatile volatile
+#define uwolatile volatile
+
+#define uwuunion union
+#define unionuwu union
+#define uwunion union
+#define unwion union
+#define unwyon union
+#define uniowon union
+
+#define uwustruct struct
+#define structuwu struct
+#define stwuct struct
+
+#define uwuenum enum
+#define enumuwu enum
+#define enyum enum
+#define enyaum enum
+#define ewnum enum
+
+#define uwutypedef typedef
+#define typedefuwu typedef
+#define twypedef typedef
+
+#define uwusizeof sizeof
+#define sizeofuwu sizeof
+#define sizeuwof sizeof
+#define swizeof sizeof
+
+#define uwuif if
+#define ifuwu if
+#define nyif if
+
+#define uwuelse else
+#define elseuwu else
+#define ewlse else
+#define ewse else
+#define nyelse else
+
+#define uwuswitch switch
+#define switchuwu switch
+#define switchy switch
+#define swyitch switch
+#define swnyitch switch
+
+#define uwucase case
+#define caseuwu case
+#define cawse case
+#define cayse case
+#define cnyase case
+
+#define uwudefault default
+#define defaultuwu default
+#define defauwt default
+
+#define uwufor for
+#define foruwu for
+#define fwor for
+
+#define uwuwhile while
+#define whileuwu while
+#define whiluwu while
+#define whiwe while
+
+#define uwudo do
+#define douwu do
+#define duwu do
+#define doh do
+
+#define uwubreak break
+#define breakuwu break
+#define bweak break
+
+#define uwucontinue continue
+#define continueuwu continue
+#define continuwu continue
+#define continyue continue
+
+#define uwureturn return
+#define returnuwu return
+#define retuwu return
+#define retuwn return
+#define wetuwn return
+
+#define uwugoto goto
+#define gotouwu goto
+#define gotowo goto
+
+#define uwuextern extern
+#define externuwu extern
+#define extewn extern
+#define uwextern extern
